153308a769e6308105004361295006e5c53ad227
saint <saint@10.3.61.148>
2024-12-04 13:00:53 +0100

    Création d'un nouveau projet versionné

--- src/cpo_mini_projet/InterfaceBienvenue.java
+++ src/cpo_mini_projet/InterfaceBienvenue.java
@@ -15,6 +15,15 @@ public class InterfaceBienvenue extends javax.swing.JFrame {
      */
     public InterfaceBienvenue() {
         initComponents();
+
+        // Ajouter un ActionListener au bouton
+        JButton_d_entree.addActionListener(e -> {
+            // Fermer la fenêtre actuelle
+            this.dispose();
+
+            // Ouvrir la fenêtre InterfaceChoixDifficulte
+            new InterfaceChoixDifficulte().setVisible(true);
+        });
     }
 
     /**
@@ -78,31 +87,6 @@ public class InterfaceBienvenue extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        JButton_d_entree.addActionListener(InterfaceDemineurV2.ChoisirDifficulte());
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(InterfaceBienvenue.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(InterfaceBienvenue.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(InterfaceBienvenue.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(InterfaceBienvenue.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
             new InterfaceBienvenue().setVisible(true);
         });
